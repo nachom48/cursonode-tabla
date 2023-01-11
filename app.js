471c338b8659e6ae6d1e7c1ea2a0e21684cc90f9
nachom48 <nachom48@gmail.com>
2023-01-10 23:18:39 -0300

    Mas cambios

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const argv=require('./config/yargs')
 
 console.clear()
 
-crearArchivoTabla(argv.b,argv.l)
+crearArchivoTabla(argv.b,argv.l,argv.h)
     .then(nombreArchivo =>console.log(nombreArchivo,'creado'))
     .catch(err=>console.log(err))
 
